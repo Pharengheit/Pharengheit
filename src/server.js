973744bb20cbe2b3667809acff5e6b7e5335d1e9
Pharengheit <warzealot@rambler.ru>
2020-03-27 20:33:15 +0300

    Создали контроллер пользователья

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import process from 'process';
+import express from 'express';
+import user from './routers/UserRouter.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/users', user);
+
+const server = app.listen(PORT, () =>{
+    console.log(`Server started on port ${server.address().port}`);
+});
