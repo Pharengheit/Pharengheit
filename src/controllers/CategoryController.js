1c081f9982c21c6ff21e57ff1e90aa2450add2ae
Pharengheit <warzealot@rambler.ru>
2020-04-10 12:57:10 +0300

    easy database added

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,70 +1,55 @@
 import Category from '../models/Category.js';
+import { CategoryRepository } from '../repositories/CategoryRepository.js';
 
 class CategoryController {
 
-    constructor(categories) {
-        this.get = this.get.bind(this);
-        this.create = this.create.bind(this);
-        this.update = this.update.bind(this);
-        this.delete = this.delete.bind(this);
-        this.listAll = this.listAll.bind(this);
+  constructor(pool) {
+    this.get = this.get.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
 
-        this._categories = categories;
+    this.categotyRepository = new CategoryRepository(pool);
+  }
+
+  async get(request, response, next) {
+    response.json(await this.categotyRepository.getAllCategories());
+  }
+
+  async create(request, response, next) {
+
+    const name = request.body.name;
+
+    const category = await this.categotyRepository.createCategory(name);
+
+    response.send(category);
+  }
+
+  async update(request, response, next) {
+    const id = Number(request.params.id);
+    const name = request.body.name;
+
+    try {
+      const category = await this.categotyRepository.updateCategory({
+        id: id,
+        name: name
+      });
+      response.json(category);
+    } catch (e) {
+      response.status(500).send(e.message);
     }
+  }
 
-    get(request, response, next) {
-        response.json([...this._categories]);
+  async delete(request, response, next) {
+    const id = Number(request.params.id);
+
+    try {
+      await this.categotyRepository.deleteCategory(id);
+      response.send('ok');
+    } catch (e) {
+      response.status(500).send(e.message);
     }
-
-    create(request, response, next) {
-
-        const id = this._categories.size + 1;
-        const name = request.body.name;
-
-        const category = new Category(id, name);
-        this._categories.set(id, category);
-
-        response.send(category);
-    }
-
-    update(request, response, next) {
-
-        const id = request.params.id;
-        const category = this._categories.get(Number(id));
-
-        if (category !== undefined) {
-            const name = request.body.name;
-            category.name = name;
-
-            this._categories.set(id, category);
-            response.json(category);
-        } else {
-            throw new Error('Category not found');
-        }
-    }
-
-    delete(request, response, next) {
-        const id = request.params.id;
-        const category = this._categories.get(Number(id));
-
-        if (category !== undefined) {
-            this._categories.delete(Number(id));
-            response.send('ok');
-        } else {
-            throw new Error('Category not found');
-        }
-    }
-
-    listAll(request, response, next) {
-
-        const categories = [];
-
-        for (let cat of this._categories) {
-            categories.push(cat[1]);
-        }
-
-        response.render('index', {'categories': categories})
-    }
+  }
 }
 
 export default CategoryController;
